d526503d6d345103e57c52822cd2998e4d7a44ba
Max Vigueras Tovar <mvt.2000@hotmail.com>
2024-07-22 16:43:17 -0600

    Products: Rules & model added

---
+++ app/Http/Requests/Product/StoreRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|unique:products|max:255',
+            'image' => 'required|dimensions:min_width:100,min_height:200',
+            'sell_price' => 'required',
+            'category_id' => 'integer|required|exists:App\Models\Category,id',
+            'provider_id' => 'integer|required|exists:App\Models\Provider,id',
+        ];
+    }
+
+    public function messages(){
+        return [
+            'name.required' => 'El campo es requerido',
+            'name.string' => 'El valor no es correcto',
+            'name.unique' => 'El producto ya está registrado',
+            'name.max' => 'Solo se permiten 255 caracteres',
+
+            'image.required' => 'El campo es requerido',
+            'image.dimensions' => 'Solo se permiten imagenes de 100x200 px',
+
+            'sell_price.required' => 'El campo es requerido',
+
+            'category_id.integer' => 'El valor debe ser un número entero',
+            'category_id.required' => 'El campo es requerido',
+            'category_id.exists' => 'La categoría no existe',
+
+            'provider_id.integer' => 'El valor debe ser un número entero',
+            'provider_id.required' => 'El campo es requerido',
+            'provider_id.exists' => 'El proveedor no existe',
+        ];
+    }
+
+}
